c4737419a58223b5d4bd876a7cba5221be532a91
dansanti <dansanti@mail.com>
2016-12-09 05:25:06 -0300

    [FIX] Mal redondeo, Es mejor tomar los valores iva incluido

--- models/point_of_sale.py
+++ models/point_of_sale.py
@@ -1694,3 +1694,14 @@ www.sii.cl'''.format(folio, folio_inicial, folio_final)
             'target': 'current',
         }
         return abs
+
+    @api.depends('statement_ids', 'lines.price_subtotal_incl', 'lines.discount')
+    def _compute_amount_all(self):
+        for order in self:
+            order.amount_paid = order.amount_return = order.amount_tax = 0.0
+            currency = order.pricelist_id.currency_id
+            order.amount_paid = sum(payment.amount for payment in order.statement_ids)
+            order.amount_return = sum(payment.amount < 0 and payment.amount or 0 for payment in order.statement_ids)
+            order.amount_tax = currency.round(sum(self._amount_line_tax(line, order.fiscal_position_id) for line in order.lines))
+            amount_total = currency.round(sum(line.price_subtotal_incl for line in order.lines))
+            order.amount_total = amount_total
