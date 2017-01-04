3ae242cbfef9fb02236f5c1baf9e31f7cac3e29d
dansanti <dansanti@mail.com>
2017-01-04 08:07:11 -0300

    [IMP]Estructura simple para consulta de folios[WIP]

--- controllers/boleta.py
+++ controllers/boleta.py
@@ -7,12 +7,22 @@ _logger = logging.getLogger(__name__)
 
 class Boleta(http.Controller):
 
+    @http.route(['/boleta'], type='http', auth="public", website=True)
+    def input_document(self, **post):
+        if not 'boleta' in post:
+            return request.website.render('l10n_cl_dte_point_of_sale.website_layout_input')
+        return request.redirect('/boleta/%s' %(post['boleta']))
+
     @http.route(['/boleta/<int:folio>'], type='http', auth="public", website=True)
-    def download_document(self, folio=None, **kw):
+    def download_document(self, folio=None, **post):
+        if 'otra_boleta' in post:
+            return request.redirect('/boleta/%s' %(post['otra_boleta']))
         Model = request.registry['pos.order']
         cr, uid, context = request.cr, request.uid, request.context
         res = Model.search(cr, SUPERUSER_ID, [('sii_document_number', '=', folio ),
-                                              ('document_class_id.sii_code', 'in', [39, 41] )], context=context)
+                                              ('document_class_id.sii_code', 'in', [39, 41] )],
+                                            limit=1,
+                                            context=context)
         orders  = Model.browse(cr, SUPERUSER_ID, res, context=context)
         values = {
             'docs': orders,
