2f0ad8909c52b13bc4ea9712b9c0d0f5636dc589
dansanti <dansanti@mail.com>
2017-01-22 23:34:36 -0300

    [FIX]Documento Recibido por el SII pero Datos NO Coinciden              con los registrados El error era dado por el dígito verificado del receptor, que era  mal entregado, y valor total 1ue llevabaun .0

--- models/point_of_sale.py
+++ models/point_of_sale.py
@@ -1467,9 +1467,17 @@ www.sii.cl'''.format(folio, folio_inicial, folio_final)
         date_order = util_model._change_time_zone(datetime.strptime(self.date_order, DTF), from_zone, to_zone).strftime(DTF)[:10]
         date_invoice = datetime.strptime(date_order, "%Y-%m-%d").strftime("%d-%m-%Y")
         rut = signature_d['subject_serial_number']
-        respuesta = _server.getEstDte(rut[:8], str(rut[-1]),
-                self.company_id.vat[2:-1],self.company_id.vat[-1], receptor[:8],receptor[2:-1],str(self.document_class_id.sii_code), str(self.sii_document_number),
-                date_invoice, str(self.amount_total),token)
+        respuesta = _server.getEstDte(rut[:8],
+                                      str(rut[-1]),
+                                      self.company_id.vat[2:-1],
+                                      self.company_id.vat[-1],
+                                      receptor[:8],
+                                      receptor[-1],
+                                      str(int(self.document_class_id.sii_code)),
+                                      str(self.sii_document_number),
+                                      date_invoice,
+                                      str(self.amount_total),
+                                      token)
         self.sii_message = respuesta
         resp = xmltodict.parse(respuesta)
         if resp['SII:RESPUESTA']['SII:RESP_HDR']['ESTADO'] == '2':
