b2d3d8821ce49e26a651d443738bc98467baaa3f
dansanti <dansanti@mail.com>
2017-02-13 15:47:41 -0300

    [FIX]Asigna siempre boleta Cuando se hace click en factura

--- static/src/js/pos_dte.js
+++ static/src/js/pos_dte.js
@@ -104,7 +104,10 @@ odoo.define('l10n_cl_dte_point_of_sale.pos_dte', function (require) {
     },
     click_invoice: function(){
       this._super();
-      this.click_boleta();
+      var order = this.pos.get_order();
+      if (order.es_boleta()){
+        this.click_boleta();
+      }
     },
     click_boleta: function(){
           var order = this.pos.get_order();
@@ -256,118 +259,118 @@ odoo.define('l10n_cl_dte_point_of_sale.pos_dte', function (require) {
    },
    validar_rut: function(texto)
    {
-     	var tmpstr = "";
-     	for ( i=0; i < texto.length ; i++ )
-     		if ( texto.charAt(i) != ' ' && texto.charAt(i) != '.' && texto.charAt(i) != '-' )
-     			tmpstr = tmpstr + texto.charAt(i);
-     	texto = tmpstr;
-     	var largo = texto.length;
+      var tmpstr = "";
+      for ( i=0; i < texto.length ; i++ )
+        if ( texto.charAt(i) != ' ' && texto.charAt(i) != '.' && texto.charAt(i) != '-' )
+          tmpstr = tmpstr + texto.charAt(i);
+      texto = tmpstr;
+      var largo = texto.length;
 
-     	if ( largo < 2 )
-     	{
-     		this.gui.show_popup('error',_t('Debe ingresar el rut completo'));
-     		return false;
-     	}
+      if ( largo < 2 )
+      {
+        this.gui.show_popup('error',_t('Debe ingresar el rut completo'));
+        return false;
+      }
 
-     	for (i=0; i < largo ; i++ )
-     	{
-     		if ( texto.charAt(i) !="0" && texto.charAt(i) != "1" && texto.charAt(i) !="2" && texto.charAt(i) != "3" && texto.charAt(i) != "4" && texto.charAt(i) !="5" && texto.charAt(i) != "6" && texto.charAt(i) != "7" && texto.charAt(i) !="8" && texto.charAt(i) != "9" && texto.charAt(i) !="k" && texto.charAt(i) != "K" )
-      		{
-     			this.gui.show_popup('error',_t('El valor ingresado no corresponde a un R.U.T valido'));
-     			return false;
-     		}
-     	}
+      for (i=0; i < largo ; i++ )
+      {
+        if ( texto.charAt(i) !="0" && texto.charAt(i) != "1" && texto.charAt(i) !="2" && texto.charAt(i) != "3" && texto.charAt(i) != "4" && texto.charAt(i) !="5" && texto.charAt(i) != "6" && texto.charAt(i) != "7" && texto.charAt(i) !="8" && texto.charAt(i) != "9" && texto.charAt(i) !="k" && texto.charAt(i) != "K" )
+          {
+          this.gui.show_popup('error',_t('El valor ingresado no corresponde a un R.U.T valido'));
+          return false;
+        }
+      }
       var j =0;
-     	var invertido = "";
-     	for ( i=(largo-1),j=0; i>=0; i--,j++ )
-     		invertido = invertido + texto.charAt(i);
-     	var dtexto = "";
-     	dtexto = dtexto + invertido.charAt(0);
-     	dtexto = dtexto + '-';
-     	var cnt = 0;
+      var invertido = "";
+      for ( i=(largo-1),j=0; i>=0; i--,j++ )
+        invertido = invertido + texto.charAt(i);
+      var dtexto = "";
+      dtexto = dtexto + invertido.charAt(0);
+      dtexto = dtexto + '-';
+      var cnt = 0;
 
-     	for ( i=1, j=2; i<largo; i++,j++ )
-     	{
-     		//alert("i=[" + i + "] j=[" + j +"]" );
-     		if ( cnt == 3 )
-     		{
-     			dtexto = dtexto + '.';
-     			j++;
-     			dtexto = dtexto + invertido.charAt(i);
-     			cnt = 1;
-     		}
-     		else
-     		{
-     			dtexto = dtexto + invertido.charAt(i);
-     			cnt++;
-     		}
-     	}
+      for ( i=1, j=2; i<largo; i++,j++ )
+      {
+        //alert("i=[" + i + "] j=[" + j +"]" );
+        if ( cnt == 3 )
+        {
+          dtexto = dtexto + '.';
+          j++;
+          dtexto = dtexto + invertido.charAt(i);
+          cnt = 1;
+        }
+        else
+        {
+          dtexto = dtexto + invertido.charAt(i);
+          cnt++;
+        }
+      }
 
-     	invertido = "";
-     	for ( i=(dtexto.length-1),j=0; i>=0; i--,j++ )
-     		invertido = invertido + dtexto.charAt(i);
-     	if ( this.revisarDigito2(texto) )
-     		return true;
+      invertido = "";
+      for ( i=(dtexto.length-1),j=0; i>=0; i--,j++ )
+        invertido = invertido + dtexto.charAt(i);
+      if ( this.revisarDigito2(texto) )
+        return true;
 
-     	return false;
+      return false;
     },
      revisarDigito: function( dvr )
     {
-    	var dv = dvr + ""
-    	if ( dv != '0' && dv != '1' && dv != '2' && dv != '3' && dv != '4' && dv != '5' && dv != '6' && dv != '7' && dv != '8' && dv != '9' && dv != 'k'  && dv != 'K')
-    	{
-    		this.gui.show_popup('error',_t('Debe ingresar un digito verificador valido'));
-    		return false;
-    	}
-    	return true;
+      var dv = dvr + ""
+      if ( dv != '0' && dv != '1' && dv != '2' && dv != '3' && dv != '4' && dv != '5' && dv != '6' && dv != '7' && dv != '8' && dv != '9' && dv != 'k'  && dv != 'K')
+      {
+        this.gui.show_popup('error',_t('Debe ingresar un digito verificador valido'));
+        return false;
+      }
+      return true;
     },
     revisarDigito2: function( crut )
     {
-    	var largo = crut.length;
-    	if ( largo < 2 )
-    	{
-    		this.gui.show_popup('error',_t('Debe ingresar el rut completo'));
-    		return false;
-    	}
-    	if ( largo > 2 )
-    		var rut = crut.substring(0, largo - 1);
-    	else
-    		var rut = crut.charAt(0);
-    	var dv = crut.charAt(largo-1);
-    	this.revisarDigito( dv );
+      var largo = crut.length;
+      if ( largo < 2 )
+      {
+        this.gui.show_popup('error',_t('Debe ingresar el rut completo'));
+        return false;
+      }
+      if ( largo > 2 )
+        var rut = crut.substring(0, largo - 1);
+      else
+        var rut = crut.charAt(0);
+      var dv = crut.charAt(largo-1);
+      this.revisarDigito( dv );
 
-    	if ( rut == null || dv == null )
-    		return 0
+      if ( rut == null || dv == null )
+        return 0
 
-    	var dvr = '0'
-    	var suma = 0
-    	var mul  = 2
+      var dvr = '0'
+      var suma = 0
+      var mul  = 2
 
-    	for (i= rut.length -1 ; i >= 0; i--)
-    	{
-    		suma = suma + rut.charAt(i) * mul
-    		if (mul == 7)
-    			mul = 2
-    		else
-    			mul++
-    	}
-    	var res = suma % 11
-    	if (res==1)
-    		dvr = 'k'
-    	else if (res==0)
-    		dvr = '0'
-    	else
-    	{
-    		var dvi = 11-res
-    		dvr = dvi + ""
-    	}
-    	if ( dvr != dv.toLowerCase() )
-    	{
-    		this.gui.show_popup('error',_t('EL rut es incorrecto'));
-    		return false
-    	}
+      for (i= rut.length -1 ; i >= 0; i--)
+      {
+        suma = suma + rut.charAt(i) * mul
+        if (mul == 7)
+          mul = 2
+        else
+          mul++
+      }
+      var res = suma % 11
+      if (res==1)
+        dvr = 'k'
+      else if (res==0)
+        dvr = '0'
+      else
+      {
+        var dvi = 11-res
+        dvr = dvi + ""
+      }
+      if ( dvr != dv.toLowerCase() )
+      {
+        this.gui.show_popup('error',_t('EL rut es incorrecto'));
+        return false
+      }
 
-    	return true
+      return true
     },
 
   });
