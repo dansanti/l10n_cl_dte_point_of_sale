6b3d5194c908a721825162336449bc9cafa14e5e
dansanti <dansanti@gmail.com>
2017-06-24 22:35:13 -0400

    [FIX]Errores de escritura en sintaxis

--- __openerp__.py
+++ __openerp__.py
@@ -22,7 +22,7 @@ Chile: API and GUI to access Electronic Invoicing webservices for Point of Sale.
         'point_of_sale',
         'report',
         'l10n_cl_libro_compra_venta',
-        'webiste',
+        'website',
         ],
     'external_dependencies': {
         'python': [
