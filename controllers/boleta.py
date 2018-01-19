359fdc06995b4256b03c0e94d9335f2eade9d968
celm1990 <celm1990@hotmail.com>
2018-01-19 18:30:38 -0500

    IMP: nuevo diseño de la boleta en el portal FIX: no depender de website, en V11 es posible hacerlo desde el portal

--- controllers/boleta.py
+++ controllers/boleta.py
@@ -1,30 +1,64 @@
-from odoo import SUPERUSER_ID
+# -*- coding: utf-8 -*-
+
+import urllib
+
 from odoo import models, http, api
 from odoo.http import request
 from odoo.addons.web.controllers.main import serialize_exception, content_disposition
-import logging
-_logger = logging.getLogger(__name__)
+from odoo.tools.misc import formatLang
 
 class Boleta(http.Controller):
 
     @http.route(['/boleta'], type='http', auth="public", website=True)
     def input_document(self, **post):
         if not 'boleta' in post:
-            return request.website.render('l10n_cl_dte_point_of_sale.website_layout_input')
-        return request.redirect('/boleta/%s' %(post['boleta']))
+            return request.render('l10n_cl_dte_point_of_sale.boleta_layout')
+        return request.redirect('/boleta/%s?%s' % (post['boleta'], urllib.parse.urlencode(post)))
 
     @http.route(['/boleta/<int:folio>'], type='http', auth="public", website=True)
     def download_document(self, folio=None, **post):
         if 'otra_boleta' in post:
             return request.redirect('/boleta/%s' %(post['otra_boleta']))
-        Model = request.registry['pos.order']
-        cr, uid, context = request.cr, request.uid, request.context
-        res = Model.search(cr, SUPERUSER_ID, [('sii_document_number', '=', folio ),
-                                              ('document_class_id.sii_code', 'in', [39, 41] )],
-                                            limit=1,
-                                            context=context)
-        orders  = Model.browse(cr, SUPERUSER_ID, res, context=context)
+        Model = request.env['pos.order'].sudo()
+        domain = [('sii_document_number', '=', int(folio))]
+        if post.get('date_invoice', ''):
+            domain.append(('date_order','=',post.get('date_invoice', '')))
+        if post.get('amount_total', ''):
+            domain.append(('amount_total','=',float(post.get('amount_total', ''))))
+        if post.get('sii_codigo', ''):
+            domain.append(('document_class_id.sii_code','=',int(post.get('sii_codigo', ''))))
+        else:
+            domain.append(('document_class_id.sii_code', 'in', [39, 41] ))
+        orders = Model.search(domain, limit=1)
+        if not orders:
+            Model = request.env['account.invoice'].sudo()
+            domain = [('sii_document_number', '=', folio)]
+            if post.get('date_invoice', ''):
+                domain.append(('date_invoice','=',post.get('date_invoice', '')))
+            if post.get('amount_total', ''):
+                domain.append(('amount_total','=',post.get('amount_total', '')))
+            if post.get('sii_codigo', ''):
+                domain.append(('sii_document_class_id.sii_code','=',post.get('sii_codigo', '')))
+            else:
+                domain.append(('sii_document_class_id.sii_code', 'in', [39, 41] ))
+            orders = Model.search(domain, limit=1)
         values = {
             'docs': orders,
+            'formatLang': formatLang,
+            'print_error': not bool(orders),
         }
-        return request.website.render('l10n_cl_dte_point_of_sale.website_layout', values)
+        return request.render('l10n_cl_dte_point_of_sale.boleta_layout', values)
+
+    @http.route(['/download/boleta'], type='http', auth="public", website=True)
+    def download_boleta(self, **post):
+        document = request.env[post['model']].sudo().browse(int(post['model_id']))
+        file_name = "Boleta Electronica %s" % (document.sii_document_number)
+        if document._name == 'account.invoice':
+            pdf = request.env['report'].sudo().get_pdf([document.id], 'intersport_account.report_boleta')
+        else:
+            pdf = request.env['report'].sudo().get_pdf([document.id], 'intersport_pos.report_pos_boleta_ticket')
+        pdfhttpheaders = [
+            ('Content-Type', 'application/pdf'), ('Content-Length', len(pdf)),
+            ('Content-Disposition', 'attachment; filename=%s.pdf;' % file_name)
+        ]
+        return request.make_response(pdf, headers=pdfhttpheaders)
